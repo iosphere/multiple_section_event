3f2b395e596d25d09a8975f4892e8a5679e6953e
Jonas Coch <jonas@klaftertief.de>
2012-09-16 20:41:26 +0200

    Added basic implementation of links between multiple entries

--- events/event.multiple_section.php
+++ events/event.multiple_section.php
@@ -126,21 +126,26 @@
 				}
 			}
 
+			// var_dump(self::$temporary_unresolved_links);
 			// Set up any unresolved links
 			foreach (self::$temporary_unresolved_links as $link) {
+				// var_dump($link);
 				// Determine the target index
+				// TODO Get proper target index
 				$target_index = (count($this->updated_entries[$link['target-handle']]) > 1 ? $this->updated_counts[$sectionHandle] : 0);
 
 				// Add to the section-level list of missed links
 				$local_unresolved_links[] = array(
 					'section-id' => $sectionID,
 					'target-handle' => $link['target-handle'],
-					'target-index' => $target_index,
+					// 'target-index' => $target_index,
+					'target-index' => $link['target-index'] ? $link['target-index'] : $target_index,
 					'field-name' => $link['field-name'],
 					'replacement-key' => $link['replacement-key'],
 					'this-postkey' => $sectionHandle,
 					'this-key' => $handle
 				);
+				// var_dump($local_unresolved_links);
 			}
 		}
 
@@ -163,7 +168,6 @@
 				}
 
 				// Set the replacement key to dynamically get the system:id later
-				// TODO Check how it's set
 				if (empty($matches['position'])) {
 					$replacement_key = '{@system-id:'.$matches['section'].'@}';
 				} else {
@@ -294,12 +298,14 @@
 						$this->updated_counts[$section->get('handle')] = 0;
 					}
 
-					// Stage unresolvable links locally
-					$local_unresolved_links = array();
-
 					// Get POST data for this section
 					if (self::isArraySequential(self::$post[$section->get('handle')])) {
+						// TODO Merge with single entry POST
 						foreach (self::$post[$section->get('handle')] as $position => $fields) {
+
+							// Stage unresolvable links locally
+							$local_unresolved_links = array();
+
 							if (isset($fields['system:id']) && is_numeric($fields['system:id'])) {
 								$entry_id = $fields['system:id'];
 							}
@@ -328,10 +334,25 @@
 								$success = false;
 							}
 
+							// Maintain the updates list
+							$this->updated_entries[$section->get('handle')][$position] = $entry->getAttribute("id");
+							$this->updated_counts[$section->get('handle')]++;
+
+							// Maintain the unresolved links list
+							foreach ($local_unresolved_links as $link) {
+								$link['entry-id'] = $entry->getAttribute('id');
+								$this->unresolved_links[] = $link;
+							}
+
 							$result->appendChild($entry);
 						}
 					}
 					else {
+						// TODO Merge with multiple entry POST
+
+						// Stage unresolvable links locally
+						$local_unresolved_links = array();
+
 						$fields = self::$post[$section->get('handle')];
 						$entry_id = null;
 
@@ -354,6 +375,16 @@
 
 						$success = $this->__doit($fields, $entry, null, $entry_id);
 
+						// Maintain the updates list
+						$this->updated_entries[$section->get('handle')][] = $entry->getAttribute("id");
+						$this->updated_counts[$section->get('handle')]++;
+
+						// Maintain the unresolved links list
+						foreach ($local_unresolved_links as $link) {
+							$link['entry-id'] = $entry->getAttribute('id');
+							$this->unresolved_links[] = $link;
+						}
+
 						$result->appendChild($entry);
 					}
 
@@ -362,7 +393,57 @@
 
 			// var_dump(self::$post);
 
+			// var_dump($this->unresolved_links);
+			// var_dump($this->updated_entries);
+
+			// Fix up the unresolved links
+			$this->resolveLinks();
+
 			return $result;
+		}
+
+		public function resolveLinks() {
+			// Don't bother if we're rolling back
+			// TODO implement rollback
+			if ($this->rollback) return;
+
+			foreach ($this->unresolved_links as $link) {
+				// var_dump($link);
+				// var_dump(self::$post);
+				// Fake the getSource() method
+				self::$sectionID = $link['section-id'];
+
+				$fields = array();
+
+				// Set up the ID
+				$fields['id'] = $link['entry-id'];
+
+				// var_dump($this->updated_entries);
+				// And pop in the field data
+				$value = $this->updated_entries[$link['target-handle']][$link['target-index']];
+				// var_dump($value);
+
+				if ($link['replacement-key']) {
+					// Replace the provided key with the system id
+					if ($link['target-index'] != '0') {
+						$fields[$link['this-key']] = preg_replace('/' . $link['replacement-key'] . '/', $value, self::$post[$link['this-postkey']][$link['target-index']][$link['this-key']]);
+					}
+					else {
+						$fields[$link['this-key']] = preg_replace('/' . $link['replacement-key'] . '/', $value, self::$post[$link['this-postkey']][$link['this-key']]);
+					}
+				}
+				else {
+					$fields[$link['field-name']] = $value;
+				}
+				// var_dump($fields);
+
+				// Set up dummy 
+				$result = new XMLElement('temp-' . $link['this-postkey']);
+
+				if ($value) {
+					$this->__doit($fields, $result, null, $link['entry-id']);
+				}
+			}
 		}
 
 	}
